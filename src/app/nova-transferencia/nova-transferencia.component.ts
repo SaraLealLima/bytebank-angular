c5d576a2c7db98a8dbd42edd77243a74756f070a
SaraLealLima <saral.limasm@gmail.com>
2021-05-21 10:49:43 -0300

    Inclusão do projeto para o github

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,40 @@
+import { Transferencia } from './../models/transferencia.model';
+import { Component, Output, EventEmitter } from "@angular/core";
+import { TransferenciaService } from "../services/transferencia.service";
+import { Router } from '@angular/router';
+
+
+@Component({
+    selector: 'app-nova-transferencia',
+    templateUrl: './nova-transferencia.component.html',
+    styleUrls: ['./nova-transferencia.component.scss']
+})
+
+export class NovaTransferenciaComponent {
+
+  @Output() aoTransferir  = new EventEmitter<any>();
+
+  valor: number;
+  destino: number;
+
+  constructor(private service: TransferenciaService, private router: Router) {}
+
+  transferir() {
+    console.log("Solicitando nova transferência")
+    const valorEmitir: Transferencia = {valor: this.valor, destino: this.destino};
+
+    this.service.adicionar(valorEmitir).subscribe(
+      (resultado) => {
+      console.log(resultado);
+      this.limparCampos();
+      this.router.navigateByUrl('extrato');
+    },
+    error => console.log(error));
+  }
+
+  limparCampos() {
+    this.valor = null;
+    this.destino = null;
+  }
+
+}
